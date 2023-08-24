83d9526c9cc4400e438e04f6ed5d7c3d8459e5d1
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2023-08-24 23:23:36 +0200

    Move ajmodels back into resource folder

--- src/main/java/org/provim/animatedmobs/api/data/AjLoader.java
+++ src/main/java/org/provim/animatedmobs/api/data/AjLoader.java
@@ -3,40 +3,39 @@ package org.provim.animatedmobs.api.data;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.provim.animatedmobs.api.model.AjModel;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class AjLoader {
-    public static final File MODEL_DIR = FabricLoader.getInstance().getConfigDir().resolve("AnimatedMobs/models").toFile();
-
-    public static final Gson GSON = new GsonBuilder()
+    private static final Gson GSON = new GsonBuilder()
             .registerTypeAdapter(ResourceLocation.class, new ResourceLocation.Serializer())
             .registerTypeAdapter(Matrix4f.class, new Matrix4fDeserializer())
             .registerTypeAdapter(Vector3f.class, new Vector3fDeserializer())
             .registerTypeAdapter(Item.class, new RegistryDeserializer<>(BuiltInRegistries.ITEM))
             .create();
 
-    public static AjModel require(String name) throws IllegalArgumentException, JsonParseException {
-        if (!MODEL_DIR.exists()) {
-            MODEL_DIR.mkdirs();
+    public static AjModel require(EntityType<?> type) throws IllegalArgumentException, JsonParseException {
+        ResourceLocation id = EntityType.getKey(type);
+        String path = String.format("/ajmodels/%s/%s.json", id.getNamespace(), id.getPath());
+
+        InputStream input = AjLoader.class.getResourceAsStream(path);
+        if (input == null) {
+            throw new IllegalArgumentException("Model doesn't exist: " + path);
         }
 
-        File file = new File(MODEL_DIR, name + ".json");
-        if (!file.exists()) {
-            throw new IllegalArgumentException("Model doesn't exist: " + name);
-        }
-
-        try (Reader reader = new FileReader(file)) {
+        try (Reader reader = new InputStreamReader(input)) {
             return GSON.fromJson(reader, AjModel.class);
         } catch (Throwable throwable) {
-            throw new JsonParseException("Failed to parse model: " + name, throwable);
+            throw new JsonParseException("Failed to parse model: " + path, throwable);
         }
     }
 }
