0e0fceecdc99aefd04ad7bdb8ff530e51176bb4c
tomalbrc <tomalbrc@me.com>
2023-10-23 01:11:02 +0200

    minor fix

--- src/main/java/org/provim/nylon/holders/base/AbstractAjHolder.java
+++ src/main/java/org/provim/nylon/holders/base/AbstractAjHolder.java
@@ -99,7 +99,7 @@ public abstract class AbstractAjHolder<T extends Entity & AjEntity> extends Elem
         element.setDisplaySize(this.size.x * 2, -this.size.y - 1);
         element.setModelTransformation(ItemDisplayContext.FIXED);
         element.setInvisible(true);
-        element.setInterpolationDuration(1);
+        element.setInterpolationDuration(2);
         element.getDataTracker().set(DisplayTrackedData.TELEPORTATION_DURATION, this.parent.getTeleportDuration());
 
         ItemStack itemStack = new ItemStack(rigItem);
@@ -181,8 +181,6 @@ public abstract class AbstractAjHolder<T extends Entity & AjEntity> extends Elem
     }
 
     public void addAdditionalElement(DisplayElement element) {
-        element.setInterpolationDuration(1);
-
         this.addElement(element);
         this.additionalElements.add(element);
     }
