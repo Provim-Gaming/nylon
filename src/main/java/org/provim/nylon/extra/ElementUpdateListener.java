3f76f6ad3ef929e98b576d5e1acb0aac4028afbe
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2023-10-26 15:27:16 +0200

    Add rotated offsets and yaw rotations for regular locators.

--- src/main/java/org/provim/nylon/extra/ElementUpdateListener.java
+++ src/main/java/org/provim/nylon/extra/ElementUpdateListener.java
@@ -1,7 +1,11 @@
 package org.provim.nylon.extra;
 
+import eu.pb4.polymer.virtualentity.api.VirtualEntityUtils;
 import eu.pb4.polymer.virtualentity.api.elements.GenericEntityElement;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3f;
 import org.provim.nylon.holders.base.AbstractAjHolder;
 import org.provim.nylon.holders.wrappers.Locator;
 import org.provim.nylon.model.AjPose;
@@ -10,6 +14,8 @@ import org.provim.nylon.model.AjPose;
  * Listener for locators, updates a single GenericEntityElement
  */
 public class ElementUpdateListener implements Locator.LocatorListener {
+    protected static final float COS = Mth.cos(90F * Mth.DEG_TO_RAD);
+
     protected final GenericEntityElement element;
 
     public ElementUpdateListener(GenericEntityElement element) {
@@ -18,6 +24,38 @@ public class ElementUpdateListener implements Locator.LocatorListener {
 
     @Override
     public void update(AbstractAjHolder<?> holder, AjPose pose) {
-        this.element.setOffset(new Vec3(pose.translation()));
+        if (this.element.isSendingPositionUpdates()) {
+            Entity parent = holder.getParent();
+            Vector3f offset = pose.translation();
+            float yRot = parent.getYRot();
+
+            this.rotateOffset(offset, yRot);
+            Vec3 pos = holder.getPos().add(offset.x, offset.y, offset.z);
+
+            holder.sendPacket(VirtualEntityUtils.createMovePacket(
+                    this.element.getEntityId(),
+                    pos,
+                    pos,
+                    true,
+                    yRot,
+                    0F
+            ));
+        }
+    }
+
+    private void rotateOffset(Vector3f offset, float yRot) {
+        float radians = (yRot + 90F) * Mth.DEG_TO_RAD;
+        float cos = Mth.cos(radians);
+        float sin = Mth.sin(radians);
+
+        Vector3f vector = new Vector3f(cos, 0, sin);
+        Vector3f vector2 = new Vector3f(cos * COS, 1, sin * COS);
+        vector.cross(vector2).negate();
+
+        float offsetX = cos * offset.z + vector2.x * offset.y + vector.x * offset.x;
+        float offsetZ = sin * offset.z + vector2.z * offset.y + vector.z * offset.x;
+
+        offset.x = offsetX;
+        offset.z = offsetZ;
     }
 }
