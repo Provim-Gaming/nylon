315948d974504a644045e0f5501b19a875a30ee8
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2023-11-03 14:50:36 +0100

    Simplify calculating position offsets

--- src/main/java/org/provim/nylon/extra/ElementUpdateListener.java
+++ src/main/java/org/provim/nylon/extra/ElementUpdateListener.java
@@ -14,8 +14,6 @@ import org.provim.nylon.model.AjPose;
  * Listener for locators, updates a single GenericEntityElement
  */
 public class ElementUpdateListener implements Locator.LocatorListener {
-    protected static final float COS = Mth.cos(90F * Mth.DEG_TO_RAD);
-
     protected final GenericEntityElement element;
 
     public ElementUpdateListener(GenericEntityElement element) {
@@ -26,10 +24,10 @@ public class ElementUpdateListener implements Locator.LocatorListener {
     public void update(AbstractAjHolder<?> holder, AjPose pose) {
         if (this.element.isSendingPositionUpdates()) {
             Entity parent = holder.getParent();
-            Vector3f offset = pose.translation();
             float yRot = parent.getYRot();
+            float angle = yRot * Mth.DEG_TO_RAD;
 
-            this.rotateOffset(offset, yRot);
+            Vector3f offset = pose.translation().rotateY(-angle);
             Vec3 pos = holder.getPos().add(offset.x, offset.y, offset.z);
 
             holder.sendPacket(VirtualEntityUtils.createMovePacket(
@@ -41,21 +39,5 @@ public class ElementUpdateListener implements Locator.LocatorListener {
                     0F
             ));
         }
-    }
-
-    private void rotateOffset(Vector3f offset, float yRot) {
-        float radians = (yRot + 90F) * Mth.DEG_TO_RAD;
-        float cos = Mth.cos(radians);
-        float sin = Mth.sin(radians);
-
-        Vector3f vector = new Vector3f(cos, 0, sin);
-        Vector3f vector2 = new Vector3f(cos * COS, 1, sin * COS);
-        vector.cross(vector2).negate();
-
-        float offsetX = cos * offset.z + vector2.x * offset.y + vector.x * offset.x;
-        float offsetZ = sin * offset.z + vector2.z * offset.y + vector.z * offset.x;
-
-        offset.x = offsetX;
-        offset.z = offsetZ;
     }
 }
