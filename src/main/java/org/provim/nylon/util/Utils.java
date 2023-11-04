9246f1462f76e5bfff6210d0d309d9e31f212d3a
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2023-11-04 22:52:56 +0100

    Support for polymer's packet patcher

--- src/main/java/org/provim/nylon/util/Utils.java
+++ src/main/java/org/provim/nylon/util/Utils.java
@@ -1,5 +1,7 @@
 package org.provim.nylon.util;
 
+import eu.pb4.polymer.core.impl.networking.PacketPatcher;
+import eu.pb4.polymer.networking.api.util.ServerDynamicPacket;
 import eu.pb4.polymer.virtualentity.api.elements.InteractionElement;
 import eu.pb4.polymer.virtualentity.api.tracker.EntityTrackedData;
 import eu.pb4.polymer.virtualentity.api.tracker.InteractionTrackedData;
@@ -7,6 +9,8 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.CrashReport;
 import net.minecraft.CrashReportCategory;
 import net.minecraft.ReportedException;
+import net.minecraft.network.Connection;
+import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
@@ -24,6 +28,10 @@ import java.util.List;
 public class Utils {
     public static final String[] EMPTY_STRING_ARRAY = new String[0];
     public static final ServerGamePacketListenerImpl[] EMPTY_CONNECTION_ARRAY = new ServerGamePacketListenerImpl[0];
+
+    public static Connection getConnection(ServerCommonPacketListenerImpl networkHandler) {
+        return ((ServerCommonPacketListenerImplAccessor) networkHandler).getConnection();
+    }
 
     public static int toSlimeSize(float size) {
         return Mth.floor(size / 2.04F / 0.255F);
@@ -75,15 +83,28 @@ public class Utils {
         return list.toArray(EMPTY_STRING_ARRAY);
     }
 
-    public static void sendPacketNoFlush(ServerCommonPacketListenerImpl listener, Packet<ClientGamePacketListener> packet) {
+    /**
+     * Vanilla + Polymer copy of {@link ServerCommonPacketListenerImpl#send(Packet, PacketSendListener)} but without flushing the connection.
+     * <p>
+     * Nylon will often have to send a ton of separate packets from a different thread.
+     * Even though we always make sure to start and finish this process before player connection flushing gets resumed at the end of the game tick,
+     * the normal send method will still flush the connection for every packet, causing a significant downgrade in network performance and ping.
+     */
+    public static void sendPacketNoFlush(ServerCommonPacketListenerImpl networkHandler, Packet<ClientGamePacketListener> packet) {
+        Packet<?> modifiedPacket = PacketPatcher.replace(networkHandler, packet);
+        if (modifiedPacket instanceof ServerDynamicPacket || PacketPatcher.prevent(networkHandler, modifiedPacket)) {
+            return;
+        }
+
         try {
-            ServerCommonPacketListenerImplAccessor accessor = (ServerCommonPacketListenerImplAccessor) listener;
-            accessor.getConnection().send(packet, null, false);
+            Utils.getConnection(networkHandler).send(modifiedPacket, null, false);
         } catch (Throwable throwable) {
             CrashReport report = CrashReport.forThrowable(throwable, "Sending packet");
             CrashReportCategory category = report.addCategory("Packet being sent");
-            category.setDetail("Packet class", () -> packet.getClass().getCanonicalName());
+            category.setDetail("Packet class", () -> modifiedPacket.getClass().getCanonicalName());
             throw new ReportedException(report);
         }
+
+        PacketPatcher.sendExtra(networkHandler, packet);
     }
 }
